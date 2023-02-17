7afadd6402dc19dfae51b6a101b67d447b7fa55f
king <miabiyewisdom@gmail.com>
2023-02-16 17:21:23 -0800

    form almost complete

--- back-card/src/App.js
+++ back-card/src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React from'react'
 import './App.css';
+import bgmobile from './images/bg-main-mobile.png'
+import bgdesktop from './images/bg-main-desktop.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <div className='left-side'>
+      <picture>
+        <source media = "(min-width: 1024px)" srcSet={bgdesktop}/>
+        <img  src={bgmobile} alt=''/>
+      </picture>
+      </div>
+
+      <div className='right-side'>
+      <form>
+        <div className='form'>
+          <label>CardHolder Name</label>
+          <input type="text" 
+                  name="cardholder_name"
+                  id='cardholder_name'
+                  placeholder='eg. Jane Appleased'
+                  required
+           />
+
+          <label>Card Number</label>
+          <input type="number" 
+                  name="card_number"
+                  id='card_number'
+                  placeholder='eg. 1234 5678 9123 0000'
+                  maxLength={19}
+                  required
+           />
+          <label>EXP. Date(MM/YY)</label>
+        
+          <div className='exp-date'>
+          <input type="month" 
+                  name="expiry_date"
+                  id="expiry_date"
+                  placeholder='MM'
+                  required
+           />
+
+          <input type="month" 
+                  name="expiry_date"
+                  id="expiry_date"
+                  placeholder='YY'
+                  required
+           />
+           <div className='cvc'>
+           <label >CVC</label>
+            <input type="number" 
+                  name="cvc"
+                  id='cvc'
+                  placeholder='eg. 123'
+                  maxLength={3}
+                  required
+           />
+           </div>
+          
+          </div>
+          <button>Confirm</button>
+        </div>
+          
+      </form>
+      </div>
+      
     </div>
   );
 }
